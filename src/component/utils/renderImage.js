76ae94a1a16a282d6c34ffaea830ba2eeda0efef
Zade Viggers <zade.viggers@gmail.com>
2022-02-12 23:47:07 +1300

    Add renderImage() function
    Signed-off-by: Zade Viggers <zade.viggers@gmail.com>

---
+++ src/component/utils/renderImage.js
@@ -0,0 +1,115 @@
+import getImage from "./getImage";
+
+export default async function renderImage(
+  src,
+  alt,
+  preload,
+  loading = preload ? "eager" : "lazy",
+  decoding = "async",
+  breakpoints,
+  objectFit = "cover",
+  objectPosition = "50% 50%",
+  layout = "constrained",
+  placeholder = "blurred",
+  artDirectives,
+  format = ["avif", "webp"],
+  formatOptions = {
+    tracedSVG: {
+      function: "trace",
+    },
+  },
+  fallbackFormat,
+  includeSourceFormat = true,
+  ...configOptions
+) {
+  const label = `Image at ${src} optimized in`;
+  console.time(label);
+
+  const { uuid, images } = await getImage(
+    src,
+    format,
+    breakpoints,
+    placeholder,
+    artDirectives,
+    fallbackFormat,
+    includeSourceFormat,
+    formatOptions,
+    configOptions,
+    astroConfig.image,
+  );
+  console.timeEnd(label);
+
+  const { width } = images.at(-1).sizes;
+
+  const className = `astro-imagetools-${uuid}`;
+
+  const imagesrcset =
+    preload &&
+    images.at(-1).sources.find(({ format: fmt }) => fmt === preload)?.srcset;
+
+  const imagesizes = `(min-width: ${width}px) ${width}px, 100vw`;
+
+  const bgStyles = getBackgroundStyles(
+    images,
+    className,
+    objectFit,
+    objectPosition,
+  );
+
+  // TODO: Figure out how to add these:
+  /*
+  <link
+  rel="preload"
+  as="image"
+  imagesrcset={imagesrcset}
+  imagesizes={imagesizes}
+  />
+
+  <style set:html={bgStyles.join("\n\n")}></style>
+  */
+
+  return (() => {
+    const sources = images.map(({ media, sources, sizes }) =>
+      sources.map(({ format, src, srcset }) => {
+        console.log(sizes);
+        const renderedSizes = Object.entries(sizes)
+          .map((size) => size)
+          .join("\n");
+        return src
+          ? `<img
+  class="${className}"
+  src="${src}"
+  alt="${alt}"
+  ${renderedSizes}
+  srcset="${srcset}"
+  loading="${loading}"
+  sizes="${imagesizes}"
+  decoding="${decoding}"
+  onload="style.backgroundImage = 'none'"
+  style="${
+    layout === "fill"
+      ? `width: 100%; height: 100%;`
+      : layout === "fullWidth"
+      ? `width: 100%; height: auto;`
+      : layout === "constrained"
+      ? "max-width: 100%; height: auto;"
+      : null
+  }"
+/>`
+          : `<source
+  ${renderedSizes}
+  media="${media}"
+  type="${`image/${format}`}"
+  srcset="${srcset}"
+  sizes="${imagesizes}"
+/>`;
+      }),
+    );
+
+    const renderedSources = sources.join("\n");
+
+    return images.length > 1
+      ? `<picture>${renderedSources}</picture>`
+      : renderedSources;
+  })();
+}
