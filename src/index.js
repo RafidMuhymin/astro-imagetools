a270fa15f2a30ca20445a33715c7c47e0b56c489
RafidMuhymin <rafidmuhymin@gmail.com>
2022-02-13 00:05:03 +0600

    removed default imports for named imports

---
+++ src/index.js
@@ -0,0 +1,3 @@
+export { default as Image } from "./component/Image.astro";
+export { default as astroImagePlugin } from "./plugin/index.js";
+export { default as renderImage } from "./component/utils/renderImage.js";
