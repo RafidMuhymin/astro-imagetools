53f70fcbe13455ffabf6813b19648d1da799378e
RafidMuhymin <rafidmuhymin@gmail.com>
2022-03-28 21:37:33 +0600

    removed main js file

--- src/index.js
+++
@@ -1,3 +0,0 @@
-export { default as Image } from "./component/Image.astro";
-export { default as astroImagePlugin } from "./plugin/index.js";
-export { default as renderImage } from "./component/utils/renderImage.js";
