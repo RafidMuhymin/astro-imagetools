3d92c224d6476e27316fb37781ca3c8b40ebde82
RafidMuhymin <rafidmuhymin@gmail.com>
2022-03-30 20:12:59 +0600

    refactored code

--- src/api/renderImage.js
+++ src/api/renderImage.js
@@ -2,7 +2,7 @@
 import renderPicture from "./renderPicture";
 
 export default async function renderImage(props) {
-  const { picture, link, style } = await renderPicture(props);
+  const { link, style, picture } = await renderPicture(props);
 
   return { link, style, image: picture };
 }
