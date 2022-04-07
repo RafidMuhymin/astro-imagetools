3970e005155ba7b9b10278bd04d0fa41e150fe9c
RafidMuhymin <rafidmuhymin@gmail.com>
2022-04-08 01:01:17 +0600

    added ImageSupportDetection component

--- components/index.js
+++ components/index.js
@@ -2,3 +2,4 @@ export { default as Img } from "./Img.astro";
 export { default as Picture } from "./Picture.astro";
 export { default as BackgroundImage } from "./BackgroundImage.astro";
 export { default as BackgroundPicture } from "./BackgroundPicture.astro";
+export { default as ImageSupportDetection } from "./ImageSupportDetection.astro";
