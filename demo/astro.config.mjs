a65483cd2d75602fda52e9be79921945664d4eaf
RafidMuhymin <rafidmuhymin@gmail.com>
2022-04-15 01:04:05 +0600

    removed import.meta.url transformation

--- demo/astro.config.mjs
+++ demo/astro.config.mjs
@@ -4,15 +4,6 @@ import astroImagePlugin from "astro-imagetools/plugin";
 // https://astro.build/config
 export default defineConfig({
   vite: {
-    plugins: [
-      astroImagePlugin,
-      {
-        name: "import.meta.url-transformer",
-        transform: (code, id) => {
-          if (id.endsWith(".astro"))
-            return code.replace(/import.meta.url/g, `"${id}"`);
-        },
-      },
-    ],
+    plugins: [astroImagePlugin],
   },
 });
