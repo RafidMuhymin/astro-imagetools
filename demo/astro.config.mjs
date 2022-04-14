d52910c7f1da6461ccc1c068a7da18fbce41d68a
RafidMuhymin <rafidmuhymin@gmail.com>
2022-04-14 18:16:19 +0600

    random code change

--- demo/astro.config.mjs
+++ demo/astro.config.mjs
@@ -1,16 +1,9 @@
-import react from "@astrojs/react";
-import preact from "@astrojs/preact";
-import svelte from "@astrojs/svelte";
-import vue from "@astrojs/vue";
-import solid from "@astrojs/solid-js";
-import lit from "@astrojs/lit";
 import { defineConfig } from "astro/config";
 import astroImagePlugin from "astro-imagetools/plugin";
 
 // https://astro.build/config
 export default defineConfig({
   vite: {
-    plugins: [astroImagePlugin]
+    plugins: [astroImagePlugin],
   },
-  integrations: [react(), preact(), svelte(), vue(), solid(), lit()]
 });
