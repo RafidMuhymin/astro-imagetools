bdb3cdd573380929c4e27db2fd1b1cf92da95ecb
Ian VanSchooten <ian.vanschooten@gmail.com>
2022-05-10 22:41:41 -0400

    Add vitest and example test

---
+++ packages/astro-imagetools/api/utils/getLinkElement.test.ts
@@ -0,0 +1,15 @@
+import { describe, expect, it } from 'vitest'
+import getLinkElement from './getLinkElement';
+
+
+describe('getLinkElement', () => {
+  it('returns an empty string if preload is not set', () => {
+    const result = getLinkElement({linkAttributes: {}});
+    expect(result).toBe('');
+  })
+
+  it('returns an empty string if no images are provided', () => {
+    const result = getLinkElement({linkAttributes: {}, preload: 'webp'});
+    expect(result).toBe('');
+  })
+})
