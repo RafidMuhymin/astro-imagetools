87c7dd3734875e0c17f357c1f91aca97ee6b5da9
Ian VanSchooten <ian.vanschooten@gmail.com>
2022-05-17 21:57:34 -0400

    Allow flexible root and public dir, add tests

--- packages/astro-imagetools/api/utils/getSrcPath.js
+++ packages/astro-imagetools/api/utils/getSrcPath.js
@@ -1,31 +1,32 @@
 import fs from 'fs';
 import path from 'path';
-import { cwd } from "../../utils/runtimeChecks.js";
-import printWarning from "../../utils/printWarning.js";
 
 // To strip off params when checking for file on disk.
 const paramPattern = /\?.*/
 
+const { default: astroViteConfigs } = await import(
+  "../../astroViteConfigs.js"
+);
+
 /**
- * getSrcPath allows the use of `src` attributes relative to either the /public folder or project root.
+ * getSrcPath allows the use of `src` attributes relative to either the public folder or project root.
  * 
- * It first checks to see if the src is a file relative to the cwd (project root).  
- * If the file isn't found, it will look in the /public folder.
+ * It first checks to see if the src is a file relative to the project root.  
+ * If the file isn't found, it will look in the public folder.
  * Finally, if it still can't be found, the original input will be returned.
  */
 export function getSrcPath(src) {
   // If this is already resolved to a file, return it.
   if (fs.existsSync(src.replace(paramPattern, ''))) return src;
 
-  const rootPath = path.join(cwd, src);
+  const rootPath = path.join(astroViteConfigs.rootDir, src);
   const rootTest = rootPath.replace(paramPattern, '')
   if (fs.existsSync(rootTest)) return rootPath;
 
-  const publicPath = path.join(cwd, 'public', src);
+  const publicPath = path.join(astroViteConfigs.publicDir, src);
   const publicTest = publicPath.replace(paramPattern, '')
   if (fs.existsSync(publicTest)) return publicPath;
 
   // Fallback
-  printWarning({message: `"${src}" could not not be found at \n${rootTest} \n\tor \n${publicTest}`});
   return src;
 }
