2c2bfb6a96bb256ca203c219535a47b1944ac81f
Ian VanSchooten <ian.vanschooten@gmail.com>
2022-06-01 15:39:58 -0400

    Set left position of placeholder (#87)

--- packages/astro-imagetools/api/utils/getBackgroundStyles.js
+++ packages/astro-imagetools/api/utils/getBackgroundStyles.js
@@ -45,6 +45,7 @@ export default function getBackgroundStyles(
     .${className}::after {
       inset: 0;
       content: "";
+      left: 0;
       width: 100%;
       height: 100%;
       position: absolute;
