586cb690f6f76ee26e585b91bb56ef580bc0081b
RafidMuhymin <rafidmuhymin@gmail.com>
2022-11-24 23:21:02 +0600

    fixed click events don't get triggerred on images

--- packages/astro-imagetools/api/utils/getBackgroundStyles.js
+++ packages/astro-imagetools/api/utils/getBackgroundStyles.js
@@ -49,6 +49,7 @@ export default function getBackgroundStyles(
       width: 100%;
       height: 100%;
       position: absolute;
+      pointer-events: none;
       transition: opacity ${
         typeof fadeInTransition !== "object"
           ? "1s"
