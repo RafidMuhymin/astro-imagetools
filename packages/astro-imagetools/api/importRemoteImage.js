c52c1005d49903577db2c0e642d0ea70eb50cbf9
RafidMuhymin <rafidmuhymin@gmail.com>
2023-01-23 01:07:13 +0600

    fixed param configs not working with importRemoteImage

--- packages/astro-imagetools/api/importRemoteImage.js
+++ packages/astro-imagetools/api/importRemoteImage.js
@@ -2,9 +2,11 @@ import load from "../plugin/hooks/load.js";
 import getResolvedSrc from "./utils/getResolvedSrc.js";
 
 export default async function importRemoteImage(url) {
+  const { search } = new URL(url);
+
   const id = await getResolvedSrc(url);
 
-  const src = (await load(id)).slice(16, -1);
+  const src = (await load(id + search)).slice(16, -1);
 
   return src;
 }
