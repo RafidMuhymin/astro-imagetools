1317aaf30d898f602c191fc680fe6987754dc4c8
RafidMuhymin <rafidmuhymin@gmail.com>
2023-01-23 01:31:40 +0600

    fixed data uri param configs not working with  importRemoteImage

--- packages/astro-imagetools/api/importRemoteImage.js
+++ packages/astro-imagetools/api/importRemoteImage.js
@@ -2,9 +2,11 @@ import load from "../plugin/hooks/load.js";
 import getResolvedSrc from "./utils/getResolvedSrc.js";
 
 export default async function importRemoteImage(url) {
-  const { search } = new URL(url);
+  const { search, protocol, pathname } = new URL(url);
 
-  const id = await getResolvedSrc(url);
+  const id = await getResolvedSrc(
+    protocol === "data:" ? protocol + pathname : url
+  );
 
   const src = (await load(id + search)).slice(16, -1);
 
